f9d5b817aa28658b0d8b5b748e76ce366bb2a25e
Brandon Reyes Alvarado <67602859+Brandon0309@users.noreply.github.com>
2020-07-29 14:18:04 -0600

    Face recognition added
    Starting point with face recognition, at the time it only compare the user face with mine, so have to fix it to compare with the whole array. Still dont know how

--- 3C System.py
+++ 3C System.py
@@ -1,4 +1,83 @@
 from tkinter import *  # libraries imported :GUI
+import face_recognition
+import numpy as np
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("FaceCapture")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:  # ESC pressed
+        
+        print("Escape hit, closing...")
+        break
+
+    elif k%256 == 32: # SPACE pressed
+        
+        user_face = "user_0.png"
+        cv2.imwrite(r"user_database/"+user_face, frame)
+        print("User_0 written!")
+
+cam.release()
+cv2.destroyAllWindows()
+#---------------------------------------------------------------------------------
+#Copy paste this sentence to add more users to the data base
+#User image must be in user_database folder to follow the format
+image_of_brandon = face_recognition.load_image_file(r"user_database\Brandon Reyes.jpg")
+brandon_face_encoding = face_recognition.face_encodings(image_of_brandon)[0]
+#---------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------
+image_of_lena = face_recognition.load_image_file(r"user_database\lena.jpg")
+lena_face_encoding = face_recognition.face_encodings(image_of_lena)[0]
+#---------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------
+#Make an array to save the registered user database
+registered_users_encodings = [
+	brandon_face_encoding,
+	lena_face_encoding
+]
+#--------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------
+#Make an array to save the data of users//May use the same form to save the user information 
+registered_users_names = [
+	"Brandon",
+	"Lena"
+]
+#---------------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------------
+#This sentences will load and encode the image and face of the person trying to login
+image_of_user = face_recognition.load_image_file(r"user_database\user_0.png")
+user_face_encoding = face_recognition.face_encodings(image_of_user)[0]
+#---------------------------------------------------------------------------------
+#Compare the face encoding of the user with the ones of the database
+results = face_recognition.compare_faces(
+    [brandon_face_encoding], user_face_encoding)
+#---------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------
+#Print the results
+if results[0]:
+    print("Welcome!")
+else:
+    print("You are not in our database! Please create your profile or get in contact with support")
+#---------------------------------------------------------------------------------
+
 
 
 class Window:
